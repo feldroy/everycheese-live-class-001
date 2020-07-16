e4ed95853d28f00a87e556992ffc023edada79e7
Daniel Feldroy <daniel@feldroy.com>
2020-07-16 15:45:46 -0700

    Add Cheese model tests

--- everycheese/cheeses/migrations/0001_initial.py
+++ everycheese/cheeses/migrations/0001_initial.py
@@ -10,23 +10,70 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Cheese',
+            name="Cheese",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, editable=False, verbose_name='created')),
-                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, editable=False, verbose_name='modified')),
-                ('name', models.CharField(max_length=255, verbose_name='Name of Cheese')),
-                ('slug', autoslug.fields.AutoSlugField(editable=False, populate_from='name', unique=True, verbose_name='Cheese Address')),
-                ('description', models.TextField(blank=True, verbose_name='Description')),
-                ('firms', models.CharField(choices=[('unspecified', 'Unspecified'), ('soft', 'Soft'), ('semi-soft', 'Semi-Soft'), ('semi-hard', 'Semi-Hard'), ('hard', 'Hard')], default='unspecified', max_length=20, verbose_name='Firmness')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "created",
+                    model_utils.fields.AutoCreatedField(
+                        default=django.utils.timezone.now,
+                        editable=False,
+                        verbose_name="created",
+                    ),
+                ),
+                (
+                    "modified",
+                    model_utils.fields.AutoLastModifiedField(
+                        default=django.utils.timezone.now,
+                        editable=False,
+                        verbose_name="modified",
+                    ),
+                ),
+                (
+                    "name",
+                    models.CharField(max_length=255, verbose_name="Name of Cheese"),
+                ),
+                (
+                    "slug",
+                    autoslug.fields.AutoSlugField(
+                        editable=False,
+                        populate_from="name",
+                        unique=True,
+                        verbose_name="Cheese Address",
+                    ),
+                ),
+                (
+                    "description",
+                    models.TextField(blank=True, verbose_name="Description"),
+                ),
+                (
+                    "firmness",
+                    models.CharField(
+                        choices=[
+                            ("unspecified", "Unspecified"),
+                            ("soft", "Soft"),
+                            ("semi-soft", "Semi-Soft"),
+                            ("semi-hard", "Semi-Hard"),
+                            ("hard", "Hard"),
+                        ],
+                        default="unspecified",
+                        max_length=20,
+                        verbose_name="Firmness",
+                    ),
+                ),
             ],
-            options={
-                'abstract': False,
-            },
+            options={"abstract": False,},
         ),
     ]
